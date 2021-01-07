355e775caab1417d286ac38fbd6701abf18fa51b
MROS <yc1043@gmail.com>
2021-01-07 21:35:26 +0800

    簡單遞迴視圖

---
+++ dom/index.ts
@@ -0,0 +1,3 @@
+function createEditor(id: string): void {
+	
+}
--- react-immutable/src/tree-editor.tsx
+++ react-immutable/src/tree-editor.tsx
@@ -1,4 +1,53 @@
+import { createContext, useContext } from "react"
+
+type TreeNode = {
+    valid: Boolean,
+    name: string,
+    children: TreeNode[]
+}
+
+const initialTreeState: TreeNode = {
+    valid: true,
+    name: '世界',
+    children: [
+        {
+            valid: false,
+            name: 'xmowpaz3zujpmopgapofjo',
+            children: [],
+        },
+        {
+            valid: true,
+            name: '生物',
+            children: [
+                {
+                    valid: true,
+                    name: '哺乳類',
+                    children: [],
+                }
+            ]
+        }
+    ]
+}
+
+const TreeContext = createContext(initialTreeState);
+
+function TreeView(props: {node: TreeNode}): JSX.Element {
+    return <div>
+        <div style={{
+            color: props.node.valid ? 'black' : 'red'
+        }}>{props.node.name}</div>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+            <div style={{width: 20}}></div>
+            <div>
+                 {/* NOTE: 這個 key 選得很糟 */}
+                {props.node.children.map(child => <TreeView key={child.name} node={child}/>)}
+            </div>
+        </div>
+    </div>
+}
+
 export function TreeEditor() {
-    return <div>樹編輯器</div>
+    const root = useContext(TreeContext);
+    return <TreeView node={root}/>
 }
 
