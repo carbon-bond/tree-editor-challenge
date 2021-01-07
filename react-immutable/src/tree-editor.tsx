4a1a71cecf7e3f59a0d6354383a3ea2a3e3c1cd8
why8ther <lturtsamuel@gmail.com>
2021-01-07 22:54:29 +0800

    隨便做做

--- react-immutable/src/tree-editor.tsx
+++ react-immutable/src/tree-editor.tsx
@@ -1,53 +1,159 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState } from "react"
+import * as React from 'react';
+import { produce } from "immer";
 
-type TreeNode = {
-    valid: Boolean,
-    name: string,
-    children: TreeNode[]
+class Valid {
+    map: { [key: string]: boolean } = {};
+
+    constructor() {}
+
+    static genKey(trace: number[]) {
+        return trace.join('-');
+    }
+
+    insertErr(trace: number[]) {
+        this.map[Valid.genKey(trace)] = true;
+        return this;
+    }
+    clearErr(trace: number[]) {
+        delete this.map[Valid.genKey(trace)];
+        return this;
+    }
+    getErrs() {
+        return Object.keys(this.map);
+    }
 }
 
-const initialTreeState: TreeNode = {
-    valid: true,
-    name: '世界',
+type TreeNode = {
+    valid: Valid,
+    value: string,
+    max: number,
+    children: TreeNode[],
+    trace: number[]
+}
+
+let initialTreeState: TreeNode = {
+    valid: new Valid(),
+    value: '世界',
+    max: 5,
+    trace: [],
     children: [
         {
-            valid: false,
-            name: 'xmowpaz3zujpmopgapofjo',
+            valid: new Valid(),
+            max: 5,
+            value: 'xm',
             children: [],
+            trace: [],
         },
         {
-            valid: true,
-            name: '生物',
+            valid: new Valid(),
+            max: 3,
+            value: '生物',
+            trace: [],
             children: [
                 {
-                    valid: true,
-                    name: '哺乳類',
+                    max: 3,
+                    valid: new Valid(),
+                    value: '哺乳類',
                     children: [],
+                    trace: [],
                 }
             ]
         }
     ]
 }
+function assignTrace(root: TreeNode) {
+    function inner(cur: TreeNode, trace: number[]) {
+        cur.trace = [...trace];
+        cur.children.forEach((n, i) => {
+            inner(n, [...trace, i]);
+        });
+    }
+    inner(root, [0]);
+}
 
-const TreeContext = createContext(initialTreeState);
+function findByTrace(root: TreeNode, trace: number[]): TreeNode[] {
+    let cur = root;
+    let nodes = [root];
+    for (let i of trace.slice(1)) {
+        if (i >= cur.children.length) {
+            throw "三小";
+        }
+        cur = cur.children[i];
+        nodes.push(cur);
+    }
+    return nodes;
+}
 
-function TreeView(props: {node: TreeNode}): JSX.Element {
+assignTrace(initialTreeState);
+
+let context_obj = {
+    tree: initialTreeState,
+    setTree: (_handler: (root: TreeNode) => TreeNode) => {},
+};
+const TreeContext = createContext(context_obj);
+
+function updateTree(root: TreeNode, trace: number[], value: string) {
+    let nodes = findByTrace(root, trace).reverse();
+    let last = nodes[0];
+    last.value = value;
+    let self_err = last.value.length >= last.max;
+    if (self_err) {
+        last.valid.insertErr(trace);
+    } else {
+        last.valid.clearErr(trace);
+    }
+    for (let node of nodes.slice(1)) {
+        if (self_err) {
+            node.valid.insertErr(trace);
+        } else {
+            node.valid.clearErr(trace);
+        }
+    }
+}
+
+function TreeView(props: {node: TreeNode }): JSX.Element {
+    const { setTree } = useContext(TreeContext);
+    function ErrorPrompt() {
+        let errs = props.node.valid.getErrs();
+        if (errs.length == 0) {
+            return null;
+        }
+        return <div style={{ color: 'red' }}>
+            非法的 {errs.map(s => <span>{s},</span>)}
+        </div>;
+    }
     return <div>
-        <div style={{
-            color: props.node.valid ? 'black' : 'red'
-        }}>{props.node.name}</div>
+        <ErrorPrompt/>
+        <input type="text" value={props.node.value} onChange={e => {
+            let v = e.target.value;
+            setTree(root => {
+                let new_root = produce(root, (root) => {
+                    updateTree(root, props.node.trace, v);
+                });
+                return new_root;
+            });
+        }} />
         <div style={{display: 'flex', flexDirection: 'row'}}>
             <div style={{width: 20}}></div>
             <div>
                  {/* NOTE: 這個 key 選得很糟 */}
-                {props.node.children.map(child => <TreeView key={child.name} node={child}/>)}
+                {props.node.children.map((child, i)=> <TreeView key={i} node={child}/>)}
             </div>
         </div>
     </div>
 }
 
 export function TreeEditor() {
-    const root = useContext(TreeContext);
-    return <TreeView node={root}/>
+    function TreeEditorInner() {
+        const { tree } = useContext(TreeContext);
+        return <TreeView node={tree} />
+    }
+    let [tree, setTree] = useState(initialTreeState);
+    return <TreeContext.Provider value={{setTree: (handler) => {
+        setTree(handler);
+    }, tree}}>
+        <TreeEditorInner/>
+    </TreeContext.Provider>;
 }
 
