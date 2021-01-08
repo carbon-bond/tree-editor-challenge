39fd11e47cc848b3cec52c9af213ae03aebd9a10
MROS <yc1043@gmail.com>
2021-01-09 03:28:01 +0800

    可以修改節點名稱

--- react-immutable/src/tree-editor.tsx
+++ react-immutable/src/tree-editor.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext } from "react"
+import produce from "immer";
+import { createContext, useContext, useState } from "react"
 
 type TreeNode = {
     valid: Boolean,
@@ -29,25 +30,61 @@ const initialTreeState: TreeNode = {
     ]
 }
 
-const TreeContext = createContext(initialTreeState);
+const TreeContext = createContext<null | { tree: TreeNode, setTree:React.Dispatch<React.SetStateAction<TreeNode>>}>(null);
 
-function TreeView(props: {node: TreeNode}): JSX.Element {
+export function TreeEditor() {
+    const tree_state = useTreeState();
+    return <TreeContext.Provider value={tree_state}>
+        <TreeView node={tree_state.tree} path={[]} />
+    </TreeContext.Provider>;
+}
+
+function useTreeState() {
+    let [tree, setTree] = useState(initialTreeState);
+    return { tree, setTree };
+}
+
+function TreeView(props: { node: TreeNode, path: number[] }): JSX.Element {
+    const [editing, setEditing] = useState(false)
+    const tree_state = useContext(TreeContext);
+    if (tree_state == null) {
+        return <div>本行文字不該出現</div>
+    }
+    const onChange = (evt: { target: { value: string } }) => {
+        const new_tree = produce(tree_state.tree, draft => {
+            let cur = draft;
+            for (let br of props.path) {
+                console.log(`在 ${cur.name} 的 ${br}`);
+                cur = cur.children[br];
+            }
+            cur.name = evt.target.value;
+        });
+        console.log(JSON.stringify(new_tree));
+        tree_state.setTree(new_tree);
+    };
     return <div>
-        <div style={{
-            color: props.node.valid ? 'black' : 'red'
-        }}>{props.node.name}</div>
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-            <div style={{width: 20}}></div>
+        {
+            editing ?
+                <div>
+                    <input onChange={onChange} value={props.node.name} />
+                    <button onClick={() => { setEditing(false) }}>完成</button>
+                </div>
+                : <div>
+                    <span style={{ color: props.node.valid ? 'black' : 'red' }}>{props.node.name}</span>
+                    <button onClick={() => { setEditing(true) }}>修改</button>
+                </div>
+        }
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <div style={{ width: 20 }}></div>
             <div>
-                 {/* NOTE: 這個 key 選得很糟 */}
-                {props.node.children.map(child => <TreeView key={child.name} node={child}/>)}
+                {/* NOTE: 這個 key 選得很糟 */}
+                {props.node.children.map((child, index) => {
+                    let path = props.path.slice();
+                    path.push(index);
+                    return <TreeView key={index} node={child} path={path} />
+                })}
             </div>
         </div>
     </div>
-}
-
-export function TreeEditor() {
-    const root = useContext(TreeContext);
-    return <TreeView node={root}/>
 }
 
